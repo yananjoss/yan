7c350f55f3fc8f03539924efc4d7f5529403d0bc
yananjoss <143456700+yananjoss@users.noreply.github.com>
2023-08-28 19:04:32 -0300

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,14 @@
+def mult(a,b):
+    return a * b
+
+
+v1 = int(input("Digite o primeiro valor: "))
+v2 = int(input("Digite o segundo valor: "))
+
+resultado = mult(v1, v2)
+
+print(resultado)
+
+
+
+
